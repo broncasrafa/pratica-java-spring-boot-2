6020c996a50aec77389c398b6d8612558993b5b6
Rafael Francisco <broncasrafa@gmail.com>
2023-06-16 09:53:12 -0300

    operacao de seed data into mongodb database

---
+++ src/main/java/com/rsfrancisco/springbootmongodb/application/configuration/SeedDatabaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.rsfrancisco.springbootmongodb.application.configuration;
+
+import com.rsfrancisco.springbootmongodb.domain.entities.User;
+import com.rsfrancisco.springbootmongodb.domain.interfaces.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SeedDatabaseConfiguration implements CommandLineRunner {
+
+    @Autowired
+    private IUserRepository _userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        _userRepository.deleteAll();
+        User u1 = new User("Monica Mattos", "monica.mattos@test.com");
+        User u2 = new User("Rugal Bernstein", "rugal.bernstein@test.com");
+        User u3 = new User("Victoria Medeiros", "victoria.medeiros@test.com");
+        User u4 = new User("Kesha Linn", "kesha_nyon_@test.com");
+        User u5 = new User("Cibelly Ferreira", "cibellyferreiraprofessora_@test.com");
+        User u6 = new User("Jennifer Lawrence", "jennifer_lawrence@test.com");
+        _userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+    }
+}
